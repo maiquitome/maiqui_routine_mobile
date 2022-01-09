8c3b4854ea13eea1948387cf3b4997bd19f0ef4e
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-08 22:17:11 -0300

    :sparkles: add timeline_container

---
+++ lib/features/home/view/widgets/timeline_container.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import '/core/theme/theme.dart';
+
+class TimelineContainer extends StatelessWidget {
+  const TimelineContainer({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final Size _size = MediaQuery.of(context).size;
+
+    return Container(
+      padding: const EdgeInsets.only(left: padding, right: padding),
+      margin: EdgeInsets.only(top: _size.height * 0.2),
+      height: double.infinity,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: AppColors.background,
+        borderRadius: const BorderRadius.only(
+          topRight: Radius.circular(30),
+          topLeft: Radius.circular(30),
+        ),
+      ),
+      child: child,
+    );
+  }
+}
