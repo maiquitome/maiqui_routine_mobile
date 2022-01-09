31128ce37cba0dd1d897dc7d5c32d0adac06614e
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-08 22:17:44 -0300

    :sparkles: add circle_checkbox

---
+++ lib/core/widgets/circle_checkbox.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:maiqui_routine/core/theme/app_colors.dart';
+
+class CircleCheckbox extends StatelessWidget {
+  const CircleCheckbox({
+    Key? key,
+    required this.value,
+    required this.onTap,
+    required this.darkColor,
+    required this.lightColor,
+  }) : super(key: key);
+
+  final bool value;
+  final void Function()? onTap;
+  final Color darkColor;
+  final Color lightColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: value ? lightColor : AppColors.background,
+          border: Border.all(
+            color: lightColor,
+            width: 4,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(1.0),
+          child: value
+              ? Icon(
+                  Icons.check,
+                  size: 30.0,
+                  color: darkColor,
+                )
+              : Icon(
+                  Icons.check_box_outline_blank,
+                  size: 30.0,
+                  color: AppColors.background,
+                ),
+        ),
+      ),
+    );
+  }
+}
