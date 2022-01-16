7375b125fb7196f1dee197e32865766b7164bcd5
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-15 23:56:17 -0300

    :sparkles: add category model

---
+++ lib/features/category/model/category_model.dart
@@ -0,0 +1,11 @@
+class CategoryModel {
+  CategoryModel({
+    required this.name,
+    required this.lightColor,
+    required this.darkColor,
+  });
+
+  final String name;
+  final int lightColor;
+  final int darkColor;
+}
