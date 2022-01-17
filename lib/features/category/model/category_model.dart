41e30969f728c2af0cd5a3316a48d607dfe9ac60
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2022-01-16 21:01:08 -0300

    :sparkles: add titleColor

--- lib/features/category/model/category_model.dart
+++ lib/features/category/model/category_model.dart
@@ -3,9 +3,11 @@ class CategoryModel {
     required this.name,
     required this.lightColor,
     required this.darkColor,
+    required this.titleColor,
   });
 
   final String name;
   final int lightColor;
   final int darkColor;
+  final int titleColor;
 }
